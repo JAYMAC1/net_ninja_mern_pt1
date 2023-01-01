1c7dc5eb8cb65c3943e3dddefca2cb51ae911a16
James McNeil <mcneil_james@hotmail.com>
2023-01-01 22:55:26 +0000

    renamed file server.js (had a comma instead of a dot) Doh

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-// import express package
+// // import express package
+// const express = require('express')
+
+// // instantiate express application
+// const app = express()
+
 const express = require('express')
 
-// instantiate express application
+// express app
 const app = express()
-
 // listen for requests
 app.listen(4000, () => {
   console.log('listening on port 4000')
