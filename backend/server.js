d07add48a3a5006d0c94ba8d7d4fddaa32df6654
James McNeil <mcneil_james@hotmail.com>
2023-01-01 23:16:55 +0000

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT
 // instantiate express application
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
 // route handlers
 app.get('/', (req, res) => {
   res.json({ message: 'welcome to the app' })
