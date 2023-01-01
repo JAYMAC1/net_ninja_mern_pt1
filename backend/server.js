f156cc641698dd276594c04cadc7e0e93152d677
James McNeil <mcneil_james@hotmail.com>
2023-01-01 22:58:04 +0000

    installed nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,10 @@
-// // import express package
-// const express = require('express')
-
-// // instantiate express application
-// const app = express()
-
+// import express package
 const express = require('express')
 
-// express app
+// instantiate express application
 const app = express()
+
 // listen for requests
 app.listen(4000, () => {
-  console.log('listening on port 4000')
+  console.log('listening on port 4000!!!')
 })
