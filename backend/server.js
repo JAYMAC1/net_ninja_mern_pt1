757dc60a1312ce3b685ec2ee9fea74a2dd65a060
James McNeil <mcneil_james@hotmail.com>
2023-01-01 23:07:59 +0000

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
-// import express package
+// import express packages
+require('dotenv').config()
 const express = require('express')
 
+const PORT = process.env.PORT
 // instantiate express application
 const app = express()
 
+// route handlers
+app.get('/', (req, res) => {
+  res.json({ message: 'welcome to the app' })
+})
+
 // listen for requests
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log('listening on port 4000!!!')
 })
