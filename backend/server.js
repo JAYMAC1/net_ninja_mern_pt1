4946bd2899c692382e5903b78ec9db877e10c221
James McNeil <mcneil_james@hotmail.com>
2023-01-02 18:20:10 +0000

    added Shema and model for workouts

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT
 const app = express()
 
 // middleware
-app.use(express.json)
+app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
