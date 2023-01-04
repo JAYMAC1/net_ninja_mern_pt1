19dd3ff9c9f67a56925a421067791180999f3597
James McNeil <mcneil_james@hotmail.com>
2023-01-04 23:28:51 +0000

    created login & signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -11,7 +13,13 @@ function App() {
         <Navbar />
         <div className='pages'>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route exact path='/' element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
+          <Routes>
+            <Route path='/login' element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
