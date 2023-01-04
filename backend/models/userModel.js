b90cfaeb484608c15bd8ea01f3997cae73d0f998
James McNeil <mcneil_james@hotmail.com>
2023-01-04 16:27:19 +0000

    login controller complete

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -49,4 +49,25 @@ userSchema.statics.signup = async function (email, password) {
   return user
 }
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // Validation
+  if (!email || !password) {
+    throw Error('All fields must be filled.')
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Invalid Credentials!!')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+
+  if (!match) {
+    throw Error('Invalid Credentials!!')
+  }
+
+  return user
+}
+
 module.exports = mongoose.model('User', userSchema)
