5a70d961ae7ac7a45db5b65516b8264a4a7a4ec9
James McNeil <mcneil_james@hotmail.com>
2023-01-05 16:45:04 +0000

    protected React routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import { useAuthContext } from './hooks/useAuthContext'
+
+// hooks
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className='App'>
       <BrowserRouter>
         <Navbar />
         <div className='pages'>
           <Routes>
-            <Route exact path='/' element={<Home />} />
+            <Route
+              exact
+              path='/'
+              element={user ? <Home /> : <Navigate to='/login' />}
+            />
           </Routes>
           <Routes>
-            <Route path='/signup' element={<Signup />} />
+            <Route
+              path='/signup'
+              element={!user ? <Signup /> : <Navigate to='/' />}
+            />
           </Routes>
           <Routes>
-            <Route path='/login' element={<Login />} />
+            <Route
+              path='/login'
+              element={!user ? <Login /> : <Navigate to='/' />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
