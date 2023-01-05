628f524a93cef004b7c03c65eb5457ac023c3bed
James McNeil <mcneil_james@hotmail.com>
2023-01-05 14:17:38 +0000

    created useLogout hook

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -6,7 +6,7 @@ export const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN':
       return { user: action.payload }
-    case 'LOLOUT':
+    case 'LOGOUT':
       return { user: null }
     default:
       return state
