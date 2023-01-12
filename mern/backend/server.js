65c02b68f399e5daad40c178db2bb592981f1a32
James McNeil <mcneil_james@hotmail.com>
2023-01-12 20:33:45 +0000

    lesson 2 complete

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -1,10 +1,25 @@
 // package imports
 const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT
 
 // express app
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+
+  next()
+})
+
+// route handler
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'connected to app' })
+})
+
 // server listening
-app.listen(4000, () => {
-  console.log('Server listening on port 4000')
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`)
 })
