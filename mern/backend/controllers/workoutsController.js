76586b235a0d9cdb0ff5b02ac6ce20cf340d3249
James McNeil <mcneil_james@hotmail.com>
2023-01-15 21:02:12 +0000

    added comments to workout controller functions

--- mern/backend/controllers/workoutsController.js
+++ mern/backend/controllers/workoutsController.js
@@ -1,15 +1,24 @@
+// get all the workouts
 const getWorkouts = (req, res) => {
   res.status(200).json({ msg: 'GET workouts (controller)' })
 }
+
+// get a single workout
 const getWorkout = (req, res) => {
   res.status(200).json({ msg: 'GET workout by ID (controller)' })
 }
+
+// create a new workout
 const createWorkout = (req, res) => {
   res.status(200).json({ msg: 'POST workout (controller)' })
 }
+
+// delete a workout
 const deleteWorkout = (req, res) => {
   res.status(200).json({ msg: 'DELETE workout (controller)' })
 }
+
+// update workout
 const updateWorkout = (req, res) => {
   res.status(200).json({ msg: 'PATCH workout by ID (contoller)' })
 }
