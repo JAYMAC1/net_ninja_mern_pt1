77889a380b02cc9db838b2f51874479a5f4edb38
James McNeil <mcneil_james@hotmail.com>
2023-01-21 23:53:28 +0000

    form complete

--- mern/frontend/src/components/WorkoutForm.jsx
+++ mern/frontend/src/components/WorkoutForm.jsx
@@ -4,10 +4,33 @@ function WorkoutForm() {
   const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(title, load, reps)
+    const workout = {
+      title,
+      load,
+      reps,
+    }
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const json = await response.json()
+    if (!response.ok) {
+      setError(json.error)
+    }
+    if (response.ok) {
+      setLoad('')
+      setTitle('')
+      setReps('')
+      setError(null)
+      console.log('new workout added', json)
+    }
   }
   return (
     <form className='create' onSubmit={handleSubmit}>
