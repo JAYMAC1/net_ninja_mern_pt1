16324f2e798f03f56d753e445577bc25e76f97bc
James McNeil <mcneil_james@hotmail.com>
2023-01-21 23:46:10 +0000

    form created output to console

--- mern/frontend/src/components/WorkoutForm.jsx
+++ mern/frontend/src/components/WorkoutForm.jsx
@@ -4,11 +4,35 @@ function WorkoutForm() {
   const [title, setTitle] = useState('')
   const [load, setLoad] = useState('')
   const [reps, setReps] = useState('')
+
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(title, load, reps)
   }
-  return <div>WorkoutForm</div>
+  return (
+    <form className='create' onSubmit={handleSubmit}>
+      <h3>Add a New Workout</h3>
+      <label onsub>Exercise Title:</label>
+      <input
+        type='text'
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <label>Load (kg):</label>
+      <input
+        type='number'
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+      <label>Reps:</label>
+      <input
+        type='number'
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+      <button>Add Workout</button>
+    </form>
+  )
 }
 
 export default WorkoutForm
