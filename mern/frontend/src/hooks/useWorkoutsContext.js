63f45266cccc333101c607b043cefe6f7c0a2b55
James McNeil <mcneil_james@hotmail.com>
2023-01-22 14:12:01 +0000

    createing context and custom hook

---
+++ mern/frontend/src/hooks/useWorkoutsContext.js
@@ -0,0 +1,8 @@
+import { WorkoutsContext } from '../context/WorkoutsContext'
+import { useContext } from 'react'
+
+export const useWorkoutsContext = () => {
+  const context = useContext(WorkoutsContext)
+
+  return context
+}
