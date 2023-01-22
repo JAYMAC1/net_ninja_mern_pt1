24e22d6e10f56cee0001006ae80b3ea490c9e667
James McNeil <mcneil_james@hotmail.com>
2023-01-22 14:24:18 +0000

    created the context

--- mern/frontend/src/hooks/useWorkoutsContext.js
+++ mern/frontend/src/hooks/useWorkoutsContext.js
@@ -4,5 +4,11 @@ import { useContext } from 'react'
 export const useWorkoutsContext = () => {
   const context = useContext(WorkoutsContext)
 
+  if (!context) {
+    throw Error(
+      'useWorkoutContext must be used inside an WorkoutsContextProvider'
+    )
+  }
+
   return context
 }
