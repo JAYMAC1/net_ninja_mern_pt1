d4e944d7a135dece4464c0df83cfe69dccfc7fc6
James McNeil <mcneil_james@hotmail.com>
2023-01-23 23:41:54 +0000

    DELETE workout added to context and workout details

--- mern/frontend/src/context/WorkoutsContext.js
+++ mern/frontend/src/context/WorkoutsContext.js
@@ -10,10 +10,18 @@ export const workoutsReducer = (state, action) => {
       return {
         workouts: action.payload,
       }
+
     case 'CREATE_WORKOUT':
       return {
         workouts: [action.payload, ...state.workouts],
       }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter(
+          (workout) => workout._id !== action.payload._id
+        ),
+      }
+
     default:
       return state
   }
