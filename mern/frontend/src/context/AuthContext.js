0a4f6baef6771622ab5bc90c9e99ecce3a540ce1
James McNeil <mcneil_james@hotmail.com>
2023-01-30 14:20:27 +0000

    created AuthContext and import to index.js

---
+++ mern/frontend/src/context/AuthContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from 'react'
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      console.log('SET_WORKOUTS')
+      return {
+        user: action.payload,
+      }
+    case 'LOGOUT':
+      console.log('SET_WORKOUTS')
+      return {
+        user: null,
+      }
+
+    default:
+      return state
+  }
+}
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  })
+  console.log('AuthContext state:', state)
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
