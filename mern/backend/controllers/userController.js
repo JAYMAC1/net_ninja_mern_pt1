d448a7368cd5e7074a54f1e6b37043820dd60345
James McNeil <mcneil_james@hotmail.com>
2023-01-30 13:23:29 +0000

    npm jwt

--- mern/backend/controllers/userController.js
+++ mern/backend/controllers/userController.js
@@ -1,8 +1,16 @@
 const User = require('../Models/userModel')
+const jwt = require('jsonwebtoken')
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
+
+// user login
 const login = async (req, res) => {
   res.status(200).json({ msg: 'login user (controller)' })
 }
+
+// user signup
 const signup = async (req, res) => {
   const { email, password } = req.body
 
